e4922b556acbf2ca8538ee25789aa61249b74f5a
Illya Sukhomlyn <gollpfd@gmail.com>
2026-04-16 15:41:52 +0300

    hide lang

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,14 +27,14 @@ export function Header() {
           <Mountain size={32} />
           KAILASH
         </div>
-
+{/* 
           <button
             onClick={() => setLanguage(language === "uk" ? "ru" : "uk")}
             className="lang-button"
           >
             <Globe size={18} className="globe-icon" />
             <span className="lang-text">{language === "uk" ? "RU" : "UA"}</span>
-          </button>
+          </button> */}
       </div>
     </header>
   );
